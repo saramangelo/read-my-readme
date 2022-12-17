7025d93c8c8fa6bf3fa0945927bf967f2d79187f
saramangelo <saramangelo@gmail.com>
2022-12-17 12:52:35 -0800

    updated js questions array

--- index.js
+++ index.js
@@ -3,66 +3,59 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 // TODO: Create an array of questions for user input
 const questions = [
-  "What is your project title?",
-  "What is the description of the project?",
-  "What are the installation instructions?",
-  "What is the usage information?",
-  "What are the contribution guidelines?",
-  "What are the test instructions?",
+  {
+    type: "input",
+    message: "What is your project title?",
+    name: "title",
+  },
+  {
+      type: "input",
+      message: "What is the description of the project?",
+      name: "Description",
+    },
+    {
+      type: "input",
+      message: "What are the installation instructions?",
+      name: "Installation",
+    },
+    {
+      type: "input",
+      message: "What is the usage information?",
+      name: "Usage",
+    },
+    {
+      type: "input",
+      message: "What are the contribution guidelines?",
+      name: "Contributing",
+    },
+    {
+      type: "input",
+      message: "What are the test instructions?",
+      name: "Tests",
+    },
+  {
+    type: "checkbox",
+    message: "Which license do you want to include?",
+    name: "License",
+    choices: ['Apache License 2.0', 'GNU GPLv3', 'MIT', 'ISC']
+  },
+  {
+    type: "input",
+    message: "What is your Github username?",
+    name: "Questions",
+  },
+  {
+      type: "input",
+      message: "What is your email address?",
+      name: "Questions",
+    },
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
+  inquirer.prompt(questions)
 
-inquirer
-  .prompt([
-    {
-      type: "input",
-      message: questions[0],
-      name: "title",
-    },
-    {
-        type: "input",
-        message: questions[1],
-        name: "Description",
-      },
-      {
-        type: "input",
-        message: questions[2],
-        name: "Installation",
-      },
-      {
-        type: "input",
-        message: questions[3],
-        name: "Usage",
-      },
-      {
-        type: "input",
-        message: questions[4],
-        name: "Contributing",
-      },
-      {
-        type: "input",
-        message: questions[5],
-        name: "Tests",
-      },
-    {
-      type: "checkbox",
-      message: "Which license do you want to include?",
-      name: "License",
-      choices: ['Apache License 2.0', 'GNU GPLv3', 'MIT', 'ISC']
-    },
-    {
-      type: "input",
-      message: "What is your Github username?",
-      name: "Questions",
-    },
-    {
-        type: "input",
-        message: "What is your email address?",
-        name: "Questions",
-      },
-  ])
+
 //   .then((data) => {
 //     console.log(data);
 //     const filename = `${data.name.toLowerCase().split(' ').join('').json}`;
