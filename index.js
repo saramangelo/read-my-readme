92296b19c38e4407824257aeee8026daad8f93fe
saramangelo <saramangelo@gmail.com>
2022-12-16 22:20:22 -0800

    updated index.js

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require("fs");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    const filename = `${data.name.toLowerCase().split(' ').join('').json}`
+    fs.writeToFile(filename, JSON.stringify(data, null, '\t'))
+}
 
 // TODO: Create a function to initialize app
 function init() {}
