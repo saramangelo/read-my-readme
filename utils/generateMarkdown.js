5708a72dd8b27f2a868f9ed6e96ad1e1273f86e8
saramangelo <saramangelo@gmail.com>
2022-12-19 12:58:23 -0500

    updated each section of generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -71,6 +71,7 @@ function generateMarkdown(data) {
   If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
   
   ## Tests
+  ${data.tests}
   
   Go the extra mile and write tests for your application. Then provide examples on how to run them here.
 `;
