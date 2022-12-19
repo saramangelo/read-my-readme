04feb9b7bd86448c8a7b6652e344198066bdfb39
saramangelo <saramangelo@gmail.com>
2022-12-19 17:57:31 -0500

    updated genmdjs

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -28,27 +28,18 @@ function generateMarkdown(data) {
   ## Installation
   ${data.installation}
   
-  What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
   
   ## Usage
   ${data.usage}
-  
-  Provide instructions and examples for use. Include screenshots as needed.
-    
+   
   
   ## Credits
   ${data.credits}
   
-  List your collaborators, if any, with links to their GitHub profiles.
-  
-  If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-  
-  If you followed tutorials, include links to those here as well.
   
   ## License
   ${data.license}
   
-  The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
   
   ---
   
@@ -56,10 +47,8 @@ function generateMarkdown(data) {
   
   ## Badges
   
-  ![xxxx](https://img.shields.io/static/v1?label=<LABEL>&message=<MESSAGE>&color=<COLOR>)
-  // update with ${data.license} etc
-  
-  Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+  [![License: ${data.license}](https://img.shields.io/badge/License-${data.license}-color=<COLOR>)](https://opensource.org/licenses/${data.license})
+
   
   ## Features
   
