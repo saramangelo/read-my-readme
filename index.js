8e172d44ca1c958e6887d7eac7f6412204aacee3
saramangelo <saramangelo@gmail.com>
2022-12-19 12:55:09 -0500

    updated index.js and utils

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
 const questions = [
   {
@@ -11,43 +12,43 @@ const questions = [
   {
       type: "input",
       message: "What is the description of the project?",
-      name: "Description",
+      name: "description",
     },
     {
       type: "input",
       message: "What are the installation instructions?",
-      name: "Installation",
+      name: "installation",
     },
     {
       type: "input",
       message: "What is the usage information?",
-      name: "Usage",
+      name: "usage",
     },
     {
       type: "input",
       message: "What are the contribution guidelines?",
-      name: "Contributing",
+      name: "contributing",
     },
     {
       type: "input",
       message: "What are the test instructions?",
-      name: "Tests",
+      name: "tests",
     },
   {
     type: "checkbox",
     message: "Which license do you want to include?",
-    name: "License",
+    name: "license",
     choices: ['Apache License 2.0', 'GNU GPLv3', 'MIT', 'ISC']
   },
   {
     type: "input",
     message: "What is your Github username?",
-    name: "Questions",
+    name: "github",
   },
   {
       type: "input",
       message: "What is your email address?",
-      name: "Questions",
+      name: "email",
     },
 ];
 
@@ -55,16 +56,17 @@ const questions = [
 function writeToFile(fileName, data) {
   inquirer.prompt(questions)
 
-
-//   .then((data) => {
-//     console.log(data);
-//     const filename = `${data.name.toLowerCase().split(' ').join('').json}`;
+// data = giant object (answers from input)
+  .then((data) => {
+    console.log(data);
+    const filename = `${data.title.toLowerCase().split(' ').join('')}.md`;
     
-//     fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
-//     err ? console.error(err) : console.log("yay!")
-//   );
-// })
+    fs.writeFile(filename, generateMarkdown(data), (err) =>
+    err ? console.error(err) : console.log("yay!")
+  );
+})
 }
+writeToFile();
 // const filename = `${data.name.toLowerCase().split(' ').join('').json}`
 // fs.writeToFile(filename, JSON.stringify(data, null, '\t'))
 
